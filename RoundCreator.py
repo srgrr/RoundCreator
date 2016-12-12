0f3b554bc05e9ddcd4d8e32cb70805ab4a70da8a
bscuser <sergi9rr9r@gmail.com>
2016-12-12 08:49:45 +0100

    Reduced features

--- RoundCreator.py
+++ RoundCreator.py
@@ -11,44 +11,27 @@ if sys.platform.startswith('win'):
 else:
     scriptSuffix = 'sh'
 
-def createTemplate():
+def templateSource():
     s = "#include <bits/stdc++.h>\n"
     s+= "using namespace std;\n"
     s+= "\n"
     s+= "int main() {\n"
     s+= "    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);\n\n"
     s+= "}\n"
-    open("template.cpp", "w").write(s)
+    return s
 
-def createCompileScript():
+def compileScriptSource():
     s = "g++ main.cpp -Wall -O2 -DLOCAL -std=c++11\n"
-    open("compile."+scriptSuffix, "w").write(s)
+    return s
 
-def createTestScript():
+def testScriptSource():
     if sys.platform.startswith('win'):
         s = "a.exe < input.txt\n"
     else:
         s = "./a.out < input.txt\n"
-    open("test."+scriptSuffix, "w").write(s)
-
-def checkScripts():
-    try:
-        open("template.cpp", "r")
-    except:
-        createTemplate()
-
-    try:
-        open("compile."+scriptSuffix, "r")
-    except:
-        createCompileScript()
-
-    try:
-        open("test."+scriptSuffix, "r")
-    except:
-        createTestScript()
+    return s
 
 def main():
-    checkScripts()
     parser = argparse.ArgumentParser(description='Creates a contest folder structure')
     parser.add_argument("--name", default="myContest", type=str,
                         help="Contest name")
@@ -56,12 +39,6 @@ def main():
                         help="Number of problems")
     parser.add_argument("--until", default='e', type=str,
                         help="Letter of last problem")
-    parser.add_argument("--template", default="template.cpp", type=str,
-                        help="Template file")
-    parser.add_argument("--compile", default="compile."+scriptSuffix, type=str,
-                        help="Compile script")
-    parser.add_argument("--test", default="test."+scriptSuffix, type=str,
-                        help="Test script")
     parser.add_argument("--prompt", default=False, type=bool,
                         help="Open a console prompt (Windows only)")
     parser.add_argument("--kinder", default='bueno', type=str,
@@ -84,12 +61,16 @@ def main():
     except:
         ans = 'a'
         while not ans in ['y', 'n']:
-            ans = raw_input("Destination folder already exists, do you want to overwrite it [y|n] ")
+            ans = raw_input("Destination folder already exists, do you want to overwrite it [y|n] ").lower()
             if ans == 'n':
                 exit()
             if ans == 'y':
                 shutil.rmtree(args.name)
                 os.makedirs(args.name)
+
+    template = templateSource()
+    compiles = compileScriptSource()
+    tests    = testScriptSource()
 
     if not args.single:
         if problemCount > 26:
@@ -97,33 +78,18 @@ def main():
             problemNames = [str(x+1) for x in range(problemCount)]
         else:        
             problemNames = [chr(x + ord('a')) for x in range(problemCount)]
-        '''
-            For each problem we will copy the following:
-            - C++ template
-            - compile script
-            - test script
-            - empty input.txt
-            - empty output.txt
-        '''
-        for problemName in problemNames:
-            rootPath = os.path.join(args.name,problemName)
-            os.makedirs(rootPath)
-            shutil.copy(args.template, os.path.join(rootPath, "main.cpp"))
-            shutil.copy(args.compile, os.path.join(rootPath, "compile."+scriptSuffix))
-            shutil.copy(args.test, os.path.join(rootPath, "test."+scriptSuffix))
-            open(os.path.join(rootPath, "input.txt"), 'w')
-            open(os.path.join(rootPath, "output.txt"), 'w')
     else:
-        rootPath = args.name
-        shutil.copy(args.template, os.path.join(rootPath, "main.cpp"))
-        shutil.copy(args.compile, os.path.join(rootPath, "compile."+scriptSuffix))
-        shutil.copy(args.test, os.path.join(rootPath, "test."+scriptSuffix))
+            problemNames = ['']
+ 
+    for problemName in problemNames:
+        rootPath = os.path.join(args.name,problemName)
+        if not args.single: 
+            os.makedirs(rootPath)
+        open(os.path.join(rootPath, "main.cpp"), 'w').write(template)
+        open(os.path.join(rootPath, "compile."+scriptSuffix), 'w').write(compiles)
+        open(os.path.join(rootPath, "test."+scriptSuffix), 'w').write(tests)
         open(os.path.join(rootPath, "input.txt"), 'w')
         open(os.path.join(rootPath, "output.txt"), 'w')
-
-    os.remove(args.template)
-    os.remove(args.compile)
-    os.remove(args.test)
 
     print ("Done! Good luck and HAVE FUN!")
 
