6c46938952047b8e6eceabfc99faf3a9fcbbd63b
Sergio Rodriguez <sergio.rodriguez@bsc.es>
2017-08-17 15:41:04 +0200

    fixed stuff

--- RoundCreator/RoundCreator.py
+++ RoundCreator/RoundCreator.py
@@ -14,25 +14,43 @@ else:
 
 def compile_script_source():
     if sys.platform.startswith('win'):
-        import RoundCreator.compile_win
-        return RoundCreator.compile_win.source
-    import RoundCreator.compile_linux
-    return RoundCreator.compile_linux.source
+        try:
+            import compile_win
+        except:
+            import RoundCreator.compile_win as compile_win
+        return compile_win.source
+    try:
+        import compile_linux
+    except:
+        import RoundCreator.compile_linux as compile_linux
+    return compile_linux.source
 
 def test_script_source():
     if sys.platform.startswith('win'):
-        import RoundCreator.test_win
-        return RoundCreator.test_win.source
-    import RoundCreator.test_linux
-    return RoundCreator.test_linux.source
+        try:
+            import test_win
+        except:
+            import RoundCreator.test_win as test_win
+        return test_win.source
+    try:
+        import test_linux
+    except:
+        import RoundCreator.test_linux as test_linux
+    return test_linux.source
 
 def template_source():
-    import RoundCreator.cpp_template
-    return RoundCreator.cpp_template.source
+    try:
+        import cpp_template
+    except:
+        import RoundCreator.cpp_template as cpp_template
+    return cpp_template.source
 
 def hightail_XML_config(contest_path):
-    import RoundCreator.hightail_config
-    return RoundCreator.hightail_config.source%contest_path
+    try:
+        import hightail_config
+    except:
+        import RoundCreator.hightail_config as hightail_config
+    return hightail_config.source%contest_path
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Creates a contest folder structure')
