80b0f87286bbf8eff05768a1963e37e87c4b0ac9
Sergio Rodriguez <sergio.rodriguez@bsc.es>
2017-09-07 11:27:47 +0200

    modified cpp template

--- RoundCreator/cpp_template.py
+++ RoundCreator/cpp_template.py
@@ -6,36 +6,6 @@ using vvi = vector<vi>;
 using vll = vector<ll>;
 using vvll = vector<vll>;
 
-int next_int() {
-  int x;
-  if(scanf("%d", &x) != 1) exit(1);
-  return x;
-}
-
-ll next_ll() {
-  ll x;
-  if(scanf("%lld", &x) != 1) exit(1);
-  return x;
-}
-
-double next_double() {
-  double x;
-  if(scanf("%lf", &x) != 1) exit(1);
-  return x;
-}
-
-string next_string() {
-  static char buf[1000009];
-  if(scanf("%s", buf) != 1) exit(1);
-  return string(buf);
-}
-
-char next_char() {
-  char x;
-  if(scanf("%c", &x) != 1) exit(1);
-  return x;
-}
-
 int main() {
   ios_base::sync_with_stdio(0); cin.tie(0);
 }
