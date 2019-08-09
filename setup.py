2be263dd3515ad03c205ad6f7cc72303697c15a3
Sergio Rodriguez Guasch <sergi9rr9r@gmail.com>
2019-08-09 11:25:25 +0200

    Upgrade to version 2.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension, find_packages
 setup(
   name = "RoundCreator",
   packages = ["RoundCreator"],
-  version = "2.1.1",
+  version = "2.1.2",
   description = "A tool that creates a programming contest folder structure",
   long_description=open("README.md", "r").read(),
   long_description_content_type = "text/markdown",
