f5df8ac084ad2f36b3d7649e25fc206e3835b37d
Sergio Rodriguez <sergio.rodriguez@streamsets.com>
2019-10-21 07:08:57 -0700

    Make tests pep8 compliant

--- TestRoundCreator/TestMain.py
+++ TestRoundCreator/TestMain.py
@@ -5,14 +5,18 @@ from TestCommandLine import TestCommandLine
 from TestTemplate import TestTemplate
 from TestRoundCreator import TestRoundCreator
 
+
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestCommandLine)
-    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestTemplate))
-    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestRoundCreator))
-    
+    suite
+    .addTest(unittest.TestLoader().loadTestsFromTestCase(TestTemplate))
+    suite
+    .addTest(unittest.TestLoader().loadTestsFromTestCase(TestRoundCreator))
+
     success = unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
 
     sys.exit(0 if success else 1)
+
 
 if __name__ == "__main__":
     main()
--- TestRoundCreator/TestTemplate.py
+++ TestRoundCreator/TestTemplate.py
@@ -3,16 +3,18 @@ import RoundCreator.Template as T
 import sys
 from argparse import ArgumentTypeError
 
+
 class TestTemplate(unittest.TestCase):
 
-	def testTemplate_NoSubst_OK(self):
-		t = T.Template("Hello!")
-		self.assertEqual(t.apply(), "Hello!")
+    def testTemplate_NoSubst_OK(self):
+        t = T.Template('Hello!')
+        self.assertEqual(t.apply(), 'Hello!')
 
-	def testTemplate_Subst_OK(self):
-		t = T.Template("Hello ##PERSON##!")
-		self.assertEqual(t.apply({"##PERSON##": "Sergio"}), "Hello Sergio!")
+    def testTemplate_Subst_OK(self):
+        t = T.Template('Hello ##PERSON##!')
+        self.assertEqual(t.apply({'##PERSON##': 'Sergio'}), 'Hello Sergio!')
+
 
 if __name__ == '__main__':
-	suite = unittest.TestLoader().loadTestsFromTestCase(TestTemplate)
-	unittest.TextTestRunner(verbosity = 2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTemplate)
+    unittest.TextTestRunner(verbosity=2).run(suite)
